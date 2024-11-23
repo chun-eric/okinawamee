6e730b63942148bd376a44f1df9a44264c96a975
Eric Chun <eric.chun88800888@gmail.com>
2024-11-23 15:33:04 +0900

    Fix mobile padding on ValueBanner component

--- src/components/layout/ValueBanner/ValueBanner.jsx
+++ src/components/layout/ValueBanner/ValueBanner.jsx
@@ -2,12 +2,12 @@ import valueBannerImage from "../../../assets/images/ValueBanner/valueBannerImag
 const ValueBanner = () => {
   return (
     <div className='relative w-full flex flex-col items-center justify-start p-8 md:p-12 my-8 mx-auto'>
-      <div className='w-full p-10 md:p-16 '>
+      <div className='w-full md:p-10 md:p-16 '>
         <div className='max-w-4xl mx-auto text-center space-y-6 '>
           <h2 className='font-bold text-3xl md:text-4xl z-50 text-black'>
             WEAVING TRADITION WITH TOMORROW
           </h2>
-          <p className='text-base md:text-lg max-w-xl flex-wrap px-4 pb-3 z-50 mx-auto'>
+          <p className='text-base md:text-lg max-w-xl flex-wrap px-2 pb-3 z-50 mx-auto'>
             The original Okinawan kariyushi shirts crafted with both Aloha
             spirit and island heritage. Okinawa Mee shirts tells a story of our
             islands - from the vibrant coral reefs to the gentle ocean breezes
@@ -15,7 +15,7 @@ const ValueBanner = () => {
           </p>
         </div>
 
-        <div className='max-w-6xl mx-auto'>
+        <div className='md:max-w-4xl mx-auto w-full'>
           <img
             className='w-full h-auto transform -scale-x-100 '
             style={{ objectFit: "cover" }}
